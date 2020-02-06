e687fb1a72d5fb57c8130c34c5626be0042ae6e0
EduardoHuerta <eduardobjqc@gmail.com>
2020-02-05 18:20:55 -0700

    ProblemaDelCaballo

---
+++ src/recursion/Main.java
@@ -0,0 +1,15 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     // Abre main
+        Caballo miObjeto = new Caballo();
+        miObjeto.Recibe();
+    }
+}
+
+
+
